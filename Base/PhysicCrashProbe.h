13c5fd9c458b62302104e3e6db900e052e09fb3b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-29 15:34:36 +0200

    Create PhysicCrashProbe.h

---
+++ Base/PhysicCrashProbe.h
@@ -0,0 +1,10 @@
+//we cannot use the term h from physics here !
+//We already used models in the CG project to validate other models and h seems to propagate through the rendering process of the scene. Mathematical harmony
+//can help us jere ! 
+
+class CrashProbeH
+{
+
+public:
+  
+};
