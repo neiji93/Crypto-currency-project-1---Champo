f5d27d3f3f87aa54dae7433b67ce1a41bb8cbc0d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-29 15:31:43 +0200

    Create PureCongruenceAutomorphPrsim.h

---
+++ Base/PureCongruenceAutomorphPrsim.h
@@ -0,0 +1,10 @@
+//As I said in another repository, Prism project is not a weapon ! So, we can start from there in thi folder and for this file !
+//Plus, you have to look at my mathematical researches.
+
+class PureCongruenceAutomorphPrism
+{
+
+public:
+
+  void checkTheOneNumber(...);
+}
