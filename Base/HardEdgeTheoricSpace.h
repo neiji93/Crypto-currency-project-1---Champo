a8def62fc26f7cc606ba37bbfc4698e012178197
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-09-30 08:01:26 +0200

    Update HardEdgeTheoricSpace.h

--- Base/HardEdgeTheoricSpace.h
+++ Base/HardEdgeTheoricSpace.h
@@ -1,1 +1,18 @@
 //There must be some things hard to see in depth linked programs
+
+
+class HarEdgeTheoricSpaceTraccer
+{
+
+public:
+
+  void getTopologicalEquationsArray();
+  void chekcOpenTopology();
+
+};
+
+class ProtocolArray
+{
+
+
+};
