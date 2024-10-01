00be2b1673feeb4164def9ecd0c55dc9e2db26d9
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-01 07:30:30 +0200

    Create PreventSVBRDFTools.hlsl

---
+++ BlockChainForGRF/PreventSVBRDFTools.hlsl
@@ -0,0 +1,3 @@
+//There are some things in SVBRDF hlsl functions that I don't want to use.
+//Fortunately, I didn't start with rendering algorithms.
+//I'm not wrong ! see: https://www.google.fr/books/edition/Visual_Texture/_R4_AAAAQBAJ?hl=fr&gbpv=1&dq=SVBRDF+not+suited+BSSRDF&pg=PA21&printsec=frontcover
