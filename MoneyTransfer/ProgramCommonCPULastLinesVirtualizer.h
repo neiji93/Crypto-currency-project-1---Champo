c56f5a8c8f753115370db164f035ece7e9af0716
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-01 13:25:31 +0200

    Update ProgramCommonCPULastLinesVirtualizer.h

--- MoneyTransfer/ProgramCommonCPULastLinesVirtualizer.h
+++ MoneyTransfer/ProgramCommonCPULastLinesVirtualizer.h
@@ -1,5 +1,6 @@
 //fx,y, z) = exp( Pi* (differential / differential)) ….
 //see maths researches
+//THis is possible to design those next classes and say that a nuclear event can crash this crypto currency because.... Let me continue this topic researches !
 
 
 //we have to separate code from the next class.
