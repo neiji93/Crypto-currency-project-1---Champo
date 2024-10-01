f218f0ee5b0a40afda39822fa973c0150262882b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-01 13:23:40 +0200

    Create ProgramCommonCPULastLinesVirtualizer.h

---
+++ MoneyTransfer/ProgramCommonCPULastLinesVirtualizer.h
@@ -0,0 +1,20 @@
+//fx,y, z) = exp( Pi* (differential / differential)) ….
+//see maths researches
+
+
+//we have to separate code from the next class.
+//This class also forbid supercomputer CPU cells to be used.
+class NetworkTopologyFirstLinesFormulasTracer
+{
+
+
+};
+
+
+class ProgramCPULastLinesVirtualizer
+{
+
+public:
+
+   void cancelAfterSecondTermFormulaDifferentials(...);
+};
