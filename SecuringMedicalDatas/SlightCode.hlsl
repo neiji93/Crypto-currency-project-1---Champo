114d107153929015074367ea1c429972e4e0daeb
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-02 17:06:00 +0200

    Update SlightCode.hlsl

--- SecuringMedicalDatas/SlightCode.hlsl
+++ SecuringMedicalDatas/SlightCode.hlsl
@@ -1,1 +1,14 @@
+//Here is a modified sentence from my older stuff. It is a slighter formula
+//that state that multidimensional behaviour of OSs can be approximated by it.
+//I hope I'm not wrong...
+// Pi*(differential / differential) * exp(x^11)
+
+//This comes from a bad hacking technique I used.
+//on the other hand, exp(x^11) is not suited for basic
+//mutlidimensional datas because this function is not the most intuitive
+//thing to keep in mind (log is better).
+void approximatedWrongmultidimensionalPiMultDifferentialDontSerialize(..)
+{
+  //I can add a line from CG programs.
+}
 
