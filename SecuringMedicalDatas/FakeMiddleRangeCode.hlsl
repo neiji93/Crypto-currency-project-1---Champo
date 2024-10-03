8ed85038c70c8973b63057efc1e36e3bd6c78b87
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-03 09:19:55 +0200

    Create FakeMiddleRangeCode.hlsl

---
+++ SecuringMedicalDatas/FakeMiddleRangeCode.hlsl
@@ -0,0 +1,30 @@
+//fake code using exp(11).  see remarks : researches maths end 2024 - 2025  (let s mixed topic 1 to topic 5)
+//It will help slightly to test if some models are well suited to be used in this software.
+//Although we are using AI here, there must have been ways to use common algorithms creation  techniques.
+//But I hope that those AI techniques can reduce the size of the software.
+//As we may understand, it seems impossible to secure medical datas unless we use cloud for common persons (I can be wrong).
+
+
+void getExp11()
+{
+}
+
+//In order to test a model, since we must test them with exp(11) and because we mixed several topics,
+//we have to use exp(11) twice. Then, as we already used a function twice that cannot crash the program,
+//it seems the last lines of code must introduce exp(11) again. So, it seems that we have to "inject"
+//the user programs into last lines of this program to test if there is a problem with medical datas !
+//So, now, it was just a notice, and then, we wont have to use exp(11) anymore although it helped us twice.
+//On the other hand, common programs and non military purpose programs can be described as models with higher order
+//than exp(11).
+void injectProgramAfterMiddleRangeCodeForVeryLargeScaleHugeModelSize(...)
+{
+
+}
+
+//Im using my mugen crash sensor repository with AI here.
+//This an approximate method here because in game design, it seems that all simulation or arcade games
+//must be checked in order to choose game good dependancies !
+void approximateMugenCrashProbeTestArcadeMechanics()
+{
+
+}
