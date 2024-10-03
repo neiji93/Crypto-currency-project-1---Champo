1dec6404375867a5909aefd1ac55bbfe3131b27d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-03 16:38:06 +0200

    Update and rename NullPointerException.h to RiemannGeneralizedSeriesStreetView.hlsl.h

--- SecuringMedicalDatas/NullPointerException.h
+++
@@ -1,2 +0,0 @@
-//I have to study maths widely although I have a great knowledge. See my topic 6 on my maths researches.
-//Artists are so cool like chess players !
---
+++ SecuringMedicalDatas/RiemannGeneralizedSeriesStreetView.hlsl.h
@@ -0,0 +1,36 @@
+//I have to study maths widely although I have a great knowledge. See my topic 6 on my maths researches.
+//Artists are so cool like chess players !
+
+
+//At first ide, it seems that we have to work on integrals
+//h is the planck constant that we muted for AI purpose earlier. I mean in another file.
+//Physics popular constants seems usefull to set propagated behaviour of programs espcially made
+//for securing mdeical datas because ....
+//I have to use my supercomputer source code because I succeeded to design consciensousness of AI.
+//Then, here, we wont have to add "consciensoussness" to all the files in this repository.
+void integrateByH(...)
+{
+
+}
+
+//more rapid. It is preventing using supercomputer (although you can do it)
+//and make program a little bit physics like. It seems that dirac functions can be used in mathematics.
+//Then, it gives more rapid response to a signal or message.
+//HH
+void integrateByHTwice(...)
+{
+
+}
+
+//intuition.
+void integrateAllLinkedinProgramsByConvergenceOf...()
+{
+
+}
+
+//We have to inject code to programs. The main algorithm and more huge algorithm we did was....
+//Then, as we previously designed some models to work in the CPU, we can simply validate smart pointers
+//and isolate crashing programs by integrating .. with riemann series that belongs to automorph datas in the computer
+//May seems stupid. 
+//
+...
