7f0bf6b9b768d49af6de320f93440a699e2aabd8
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-04 10:40:22 +0200

    Create LatentVariables.hlsl

---
+++ SecuringMedicalDatas/SimulationModels/LatentVariables.hlsl
@@ -0,0 +1,17 @@
+
+//The injector of this folder is more powerfull than the scripting software I did for my video game and game engine. I mean the game engine I did few years bfore.
+//But it's not finished !
+
+//It will be good for you to read Ivan Olier stuffs.
+
+
+void linked_RK4_IncompleteAutomorphDtas_with_twoModels(....)
+{
+  //In AI, it is obvious to use basic techniques. But we can iteratively delete models and get an output of another model.
+  //This is smart because latent variables seems to be not linked with arcade video game models so much.
+  //RK4 called in different several functions or classes before can help us having a momentum. Then, we better use genetic algorithms
+  //(I have to don something better in the futur) that is also a neural network. We will use h=h*exp(h) later because we have a finite corpus.
+  
+
+}
+
