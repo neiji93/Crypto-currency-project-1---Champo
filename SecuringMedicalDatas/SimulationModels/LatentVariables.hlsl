d30385c48a039f586ee15773e2cebb3fb360bf5d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-04 15:27:01 +0200

    Update LatentVariables.hlsl

--- SecuringMedicalDatas/SimulationModels/LatentVariables.hlsl
+++ SecuringMedicalDatas/SimulationModels/LatentVariables.hlsl
@@ -11,7 +11,38 @@ void linked_RK4_IncompleteAutomorphDtas_with_twoModels(....)
   //This is smart because latent variables seems to be not linked with arcade video game models so much.
   //RK4 called in different several functions or classes before can help us having a momentum. Then, we better use genetic algorithms
   //(I have to don something better in the futur) that is also a neural network. We will use h=h*exp(h) later because we have a finite corpus.
-  
+
+
+
+
+  //First, we have to use some finite corpus theorems becauuse patching is usually not good for models nowadays.
+  //To merge results, then, we will have to inject code not in all programs but programs that we can patch
+  //with mutedH = mutedH * log(mutedH) ! This is due to the intuitiveness of log(x) and h is muted. So, 
+  //we have to test files on the OS that use smart pointers only and done with some libraries ! Not all libraries !
+  //Note that we don't have to use this formula anymore.
+  for(int i=0; i< os->getSharedLibraries().size(); i++)
+  {
+//we dont use momentum directly to prevent os patching (Im not sure)
+     for(int j=0; j< os->getSharedLibraries()[i]->getGenerativeSeriesFromNotSlightCodeAndNotFromMiddleRangecode().size(); j++) 
+     {
+        //Working with textures is a well known method.
+        Texture t = os->getSharedLibraries()[i]->getGenerativeSeriesFromNotSlightCodeAndNotFromMiddleRangecode();
+
+        //let s get an hasardous from finite corpus like complex numbers
+        u = createTexture( 3*Pi*i);
+        float3 result = t.SampleBy( u );
+
+        //We dont really need more stuffs on finite corpus.
+        if ( result.x >= circularMomentumTerm )  //It seems not secure
+        {
+            
+        }
+    }
+  }
+
+//if we output a texture, it will prevent our software to be an antivirus. and we have to mention that this is not a CG algorithm !
+return t;
+
 
 }
 
