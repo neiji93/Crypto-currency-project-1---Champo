767a84414ff8df030265fb2f2180a17a5f3e8d6b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-05 21:41:23 +0200

    Create Navigator1.h

---
+++ OS/Logarithm/Scheduller/Navigator1.h
@@ -0,0 +1,25 @@
+//As the scheduller file contains a lot of functions and as we use physics, we can approximate a navigator behaviour by
+//some secure methods because we return getExp11 instead of returning exp(11^2). It means that those methods are linked.
+//Then, Internet contents can be linked with some heat power formulas. It seems the more easiest way. Thats why we can put
+//them in a scheduller folder.
+//The graphical contents can be rendered with my game engine. we have then to create a GPU library later.
+
+void secureInternetContentsByApproximateLogX()
+{
+  //adding extra code to secure medical datas. Here, this is device commands by stack. Stacks are described in the next function
+  
+  
+  //In CG, .... (I have to check other programs)...
+
+  
+  //We can use some scheduller methods because 
+  
+}
+
+void renderFrame()
+{
+  //It seems that in the GPUs, it is best to work on stacks because it limits the numbers of computing requests (see notes of supercomput'er repository).
+  Stack s;
+  
+}
+
