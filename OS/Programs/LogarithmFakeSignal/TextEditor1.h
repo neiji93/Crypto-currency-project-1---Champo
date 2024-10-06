8c9fa048f8bf38497e989b35aeaaf8e54340f9a3
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-06 15:37:00 +0200

    Create TextEditor1.h

---
+++ OS/Programs/LogarithmFakeSignal/TextEditor1.h
@@ -0,0 +1,14 @@
+//this program is immediately the next step
+//of signals build release. to be compatible,
+//the signals must intuitively synchronize
+//due to the repository supercomputer cells 
+//architecture because devices use exponent 62.
+//and synchronizing forbid electronics to crash.
+//in physics, we dont need formulas that reject
+//constants powered by ai.
+
+void exponent62_crash_prevent( )
+{}
+
+void send_usersignal_before_expoonent_62()
+{}
