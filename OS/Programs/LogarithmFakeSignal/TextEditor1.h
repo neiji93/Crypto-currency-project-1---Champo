2dc5b2b7052bb5c7cbbc52ef1fe5ad95186861fa
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-07 13:31:45 +0200

    Update TextEditor1.h

--- OS/Programs/LogarithmFakeSignal/TextEditor1.h
+++ OS/Programs/LogarithmFakeSignal/TextEditor1.h
@@ -18,3 +18,19 @@ void send_usersignal_before_expoonent_62()
 //exponent 62, it will be cool to mimic a
 //derver that is just a function.call 
 //with containers.
+
+
+
+//containers may help the user interface that can be
+//suited for low level architectures if we don't use stacks 
+//for major user interactivity. This is the fact that 
+//we can have a random equation or function here because we 
+//enquired the way to use queues. Then, no need to check arbitrary
+//formulas.
+//By the way, here, we can have objects in memory. This function can be an
+//alternative way to smart pointers ! Let's check secure methods later !
+void checkContainerEnqueueUserRandomBehaviourOnHighLevelClassesOrObejcts( .... )
+{
+  //As we dont use stacks, datas may be dropped. Lets add a smart pointer here !
+
+}
