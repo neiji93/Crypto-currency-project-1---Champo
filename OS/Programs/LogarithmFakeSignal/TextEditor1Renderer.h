4099fb693f82fb17776cc4f75e85c7d03b24c702
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-07 15:24:03 +0200

    Create TextEditor1Renderer.h

---
+++ OS/Programs/LogarithmFakeSignal/TextEditor1Renderer.h
@@ -0,0 +1,13 @@
+
+
+//It was a good idea to keep in mind randoomly done equations or functions
+//in the full program. Now, in order to dispatch them to GPU with Planck's constant
+//and other muted constants, we need to send many signals. In order to get a non crashing
+//rendering, we have to use contracts of functions that were not internally done for pphysics and then...
+//I must stop it now although this is a way !
+//If we use physics, we have then to use exp(h^11) * hyperbolic function1 (from scratch as I'm an artist !).
+//As we did before, h^11 must be too high, then we will have to use h^2 and wwe have to synchronize signals too !
+//Plus, I have to check if all those program contains physical formulas as I said in this repository
+//at starting block !
+//This is cool that I'm a recognized artist ! In the math reseatch repository, I also claimed that I have an infinite
+//science from scratch ! haha ! lol !
