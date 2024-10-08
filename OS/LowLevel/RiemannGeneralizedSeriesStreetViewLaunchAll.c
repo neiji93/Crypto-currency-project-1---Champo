601b6e0d2c51e8323c4830391237051b9d2268fa
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-08 15:28:33 +0200

    Create RiemannGeneralizedSeriesStreetViewLaunchAll.c

---
+++ OS/LowLevel/RiemannGeneralizedSeriesStreetViewLaunchAll.c
@@ -0,0 +1,26 @@
+//see: securingmedicaldatas/riemannGeneralizedSeriesStreetView.hlsl.h
+
+
+//we have to deal with integrals to transform signals to ...
+//
+//Let s prevent SVBRDF methods to be used ! 
+//This is a possible way.
+
+void sendLightSignalsCode(...)
+{
+  diracTermPreventLocalModelsForAllTextures(....); //see: https://github.com/neiji93/Graphics-Byond-cell-shading-potorealism/blob/53cfd5e91dcf62cc798362522ff7237317b642c7/Graphics/SVBRDF/AISecondSVBRDF.hlsl
+
+  //as code is light, checking chips crash is also needed but as we synchronized in the futur some functions,
+  //we can only check now syncrhonizing methods ! This is betteer for a scheduller. As all c functions contains physics formulas,
+  //and as the cpu is now reading the hard disk and as we used dirac deltta, there is no need here to check crash !
+  //So, there are only 2 c commands here : laucnhing the scheduller and syncrhonizing with the less possible chips.
+  //hummm...
+  //Note that, then, we don t need smart pointers !
+  scheduuler* sche = (void*)malloc(...);
+  sche->launch(....);
+  sche->synchronie(...);
+
+  
+
+  
+}
