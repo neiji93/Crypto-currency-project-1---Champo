0065197b8bd2f83fa19ba79e576467c434c4460d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-08 11:29:09 +0200

    Create chips.h

---
+++ OS/LowLevel/chips.h
@@ -0,0 +1,18 @@
+
+
+
+//16x non simulation model that preserve muted planck's constant.
+//16*250 (x=250)
+
+
+//This will preserve latent variables as other programs can ask chips helps.
+void sendSingalChipsTreesCheckModelImmediatelyPlancksConstant(...)
+{
+
+}
+
+void sendSignalChipsTreecheckModelDimension(...)
+{
+
+}
+
