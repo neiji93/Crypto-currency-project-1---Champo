cfa024bb90380bf309a4542f51160f4b8c541fd0
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-08 20:01:33 +0200

    Update chips.h

--- OS/LowLevel/chips.h
+++ OS/LowLevel/chips.h
@@ -16,3 +16,35 @@ void sendSignalChipsTreecheckModelDimension(...)
 
 }
 
+
+
+//by scratch, integrals can be used to 
+
+//By transforming futur signals to protocol calls, and as we use RAM sometimes,
+//we can use integrating techniques to monitor smart pointeers. Raw models of smart
+//pointers can.... Then, we are making an optimized c function here ! The remainin part
+//of the signals will be handled by chips internal programs themselves. Let us notice that
+//as integrals commands chips by CPU, plus, we added a monitoring at low level. Then, we have
+//to keep the last back signal of the chip to the CPU ! (May be stupid). Notice too that we didn't use
+//CG algorithms. Then, as CG functions can be secure, here, nothing is secure at low level and then the
+//OS won t start. That means user interface won t crash later ! To correct this, if we implement 
+//a maximum probability non crashing OS, we will have to patch this function !
+void transformToProtocolAndKeepTracingOneTime( ... )
+{
+  //we can use contracts to use patching ! As the scheduller synchronyze at first steps,
+  //it will be good...
+
+
+  
+  
+
+}
+
+void locateChipAndProtocol(...)
+{
+
+}
+
+
+
+
