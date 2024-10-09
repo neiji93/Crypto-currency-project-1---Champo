5a72e5e523937d830de2f6720ec252ccc1d5a4a6
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-09 07:27:20 +0200

    Create protocols.h

---
+++ OS/LowLevel/protocols.h
@@ -0,0 +1,26 @@
+
+
+
+//We can use probabilities here because we already created some random functions
+//in different files if Im not wrong. Dont forget to test crash ! As we used heat transfer
+//formulas too, now, it is best to have another crash function. So what does it look like ?
+//We can go back to complex numbers and use prime numbers because ....
+//As we can see, calling this function several times with different prime numbers can help us
+//defining a probability function because we also used intagrals before. It means generalized
+//series that we can ..... (because of physics behaviour).
+//In video games, for example, quaternions are optimized.
+//To be continued...
+void crash_after_exponent62_heat(....)
+{
+
+}
+
+
+
+void get_protocol_from_crash_test_after(...)
+{
+  whiel( ... ){   //we do not have to use the scheduller because prime numbers can be used with .., and then, in physics, we have ....
+  crash_after_exponent62_heat(....)
+    }
+    
+}
