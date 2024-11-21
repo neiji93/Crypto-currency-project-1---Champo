a680f3d1a9be8b58b4282b3b99c565db542cdc14
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-11-21 10:57:27 +0100

    Create LightweightCode.hlsl

---
+++ Boot/LightweightCode.hlsl
@@ -0,0 +1,9 @@
+
+//in other projects like cgi 2024-2025,
+i used exp(0.6) because i didnt use the
+//6 number too much in the past. do here
+//we have to de'd signal and use domething
+//different than exp(0.6). zs we cant use log(x)
+//to proove that the code is periodless,
+//we will have to.....
+
