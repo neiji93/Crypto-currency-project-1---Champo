b310f9c8ed30c819cdeb25bf0b0f3baf093cfc56
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-12-08 19:29:13 +0100

    Create FollowBulletAnimals.h

---
+++ Antenna/FollowBulletAnimals.h
@@ -0,0 +1,4 @@
+//I don't want to hurt consciensousness of people nor mine nor greenpeace but here is a starting point
+//of something bigger ! If armies can create real bullets which can follow some target animals or
+//bullets that can be kept in air, there are then ways to check nuclear disaster and transmit them to
+//other softwares.
