5eab65997aef92891fccc2f485db41c500e6f91e
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-12-18 18:42:31 +0100

    Create CompileProcessAntenna.h

---
+++ OS/CompileProcessAntenna.h
@@ -0,0 +1,7 @@
+
+//based on a prevuous research:
+// https://github.com/neiji93/Trying-to-see-end-of-mathematics-esquisse.git
+//yhe first idea is that this os play too many games
+//then datas are forced to come into hd. the second idea that claim
+//that it is suited for crypto curre'cy is that the algorithmd entrred by users
+//can be various and hidden in the compile immediate process. there must br a way.
