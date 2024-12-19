e25aa646219c4db15e3fd110d124626c8318e3e5
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-12-19 06:40:34 +0100

    Update CompileProcessAntenna.h

--- OS/CompileProcessAntenna.h
+++ OS/CompileProcessAntenna.h
@@ -5,3 +5,8 @@
 //then datas are forced to come into hd. the second idea that claim
 //that it is suited for crypto curre'cy is that the algorithmd entrred by users
 //can be various and hidden in the compile immediate process. there must br a way.
+
+//accordi'g to my previous researches in math
+//which state the end of mathd, it is possible now to 
+//deal with "knowledge" and then create a maximum
+//nested pointer.
