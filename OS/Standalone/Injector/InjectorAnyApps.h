eb7fa0066597c338a2ad9cb55355b9efca111289
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-12-20 14:48:48 +0100

    Update InjectorAnyApps.h

--- OS/Standalone/Injector/InjectorAnyApps.h
+++ OS/Standalone/Injector/InjectorAnyApps.h
@@ -1,1 +1,5 @@
 
+//As champo OS was only been designed to take care of blockchain (notice that this my definition of this term
+//and not the real nowadays common term), we can now imagine that the processes runned in RAM or HardDsik can
+//be retrieved if we use signals to prevent nested pointers here. This will help the real pc user OS to catch
+//itself a firewall in the next step !
