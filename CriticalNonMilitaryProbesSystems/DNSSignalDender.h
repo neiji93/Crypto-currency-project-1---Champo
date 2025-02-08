01104b901ea65e9d6687dc8174b80785fac71015
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-08 07:16:55 +0100

    Create DNSSignalDender.h

---
+++ CriticalNonMilitaryProbesSystems/DNSSignalDender.h
@@ -0,0 +1,6 @@
+//if dns can access files this will be more secure
+//for dome of our purposes. notice that although we used ottoseconds
+//theories, we can use a treshold to dns refresh like we didnin coroutine framework.
+//this means that double integrals behaviour leads us to use ai.
+//then, the ai technology we need here is models because we dont need to check 
+//maths.
